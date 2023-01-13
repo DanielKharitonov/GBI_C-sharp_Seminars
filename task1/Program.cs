b94f3825aa2f3b478492b315970ee62b34311b3f
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-14 00:22:28 +0500

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// dotnet - проверка, все ли установлено
+// dotnet console - создание проекта
+// dotnet run - запуск проекта
+
+Console.Write("Введите целое число: "); // Вывод в консоль сообщения
+int number = Convert.ToInt32(Console.ReadLine()); // int.Parse() & Convert.ToInt32()
+int sqr = number * number;
+System.Console.WriteLine($"Квадрат числа: {sqr} вот!"); // ("Квадрат числа: + sqr + далее текст")
