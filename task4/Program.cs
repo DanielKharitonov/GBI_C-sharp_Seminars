30596ebfecab2df4d9f5891cc30a7f669d9b0b0b
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-14 00:24:13 +0500

    Add task4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (99 < numb && numb < 1000)
+{
+    Console.WriteLine("Введено трехзначное число");
+    int answer = numb % 10;
+    Console.WriteLine($"Последняя цифра числа: {answer}");
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
