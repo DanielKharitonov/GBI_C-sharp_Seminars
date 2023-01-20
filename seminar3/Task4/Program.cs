d4662bf250407a385b85b419abab54b5d334df7b
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-21 00:07:17 +0500

    Added Task4 into folder semminar3

---
+++ seminar3/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число N: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if(Number>0)
+{
+   for (int i = 1; i <= Number; i++)
+{
+    int Num = i*i;
+    System.Console.Write($"{Num} ");
+} 
+}
+else
+{
+    System.Console.WriteLine("Введено отрицательное число");
+}
