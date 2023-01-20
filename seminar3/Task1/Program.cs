dfe4ca5cd116fae950d24dadb31dc822f7e59aa0
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-21 00:06:26 +0500

    Added Task1 into folder semminar3

---
+++ seminar3/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Тернарные операторы
+
+Console.Clear();
+int x=2;
+int y=7;
+
+// Обычное решение:
+// if(x>y)
+// {
+//     System.Console.WriteLine(x+y);
+// }
+// else
+// {
+//     System.Console.WriteLine(x*y);
+// }
+
+int z=x>y ? (x+y) : (x*y);
+System.Console.WriteLine(z);
