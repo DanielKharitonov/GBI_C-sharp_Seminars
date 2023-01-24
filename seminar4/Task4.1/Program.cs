d3a1828dce6440c8b44893562b73af3ac6a7fd46
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-24 23:59:29 +0500

    Added Task4.1

---
+++ seminar4/Task4.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+int Sum(int number) // Методы писать с большой буквы
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int InputNumber(string message)
+{
+    int number;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (!(int.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+}
+
+int A = InputNumber("Введите число А: ");
+int B = InputNumber("Введите число B: ");
+
+int sumA=Sum(A);
+int sumB=Sum(B);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {A}: {sumA}");
+System.Console.WriteLine($"Сумма чисел от 1 до {B}: {sumB}");
