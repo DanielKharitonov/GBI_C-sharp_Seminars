cd665155943441d32eca50896bcbafef4f4cfe67
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-24 23:59:55 +0500

    Added Task4.3

---
+++ seminar4/Task4.3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+
+int InputNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не число");
+    }
+    return number;
+}
+
+void Factorial(int count)
+{
+    int answer = 1;
+    for (int i = 1; i < count + 1; i++)
+    {
+        answer *= i;
+    }
+    System.Console.WriteLine(answer);
+}
+
+int N = InputNumber("Введите число N:");
+Factorial(N);
