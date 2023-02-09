0a7a671a0567d9b70544014adf2462722f5a1361
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-02-09 21:27:00 +0500

    Added Task7.2

---
+++ seminar7/Task7.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 48: Задайте двемерный массив размера m на n,
+// каждый элемент в массиве находится по формуле:
+// Amn = m+n. Выведите полученный массив на экран.
+
+Console.Clear();
+
+int[,] MatrixSumIndex (int rows, int cols)
+{
+    int[,] matrix = new int [rows,cols];
+
+for (int i=0; i<rows;i++)
+{
+    for (int j=0;j<cols;j++)
+    {
+        matrix[i,j]=i+j;
+    }
+}
+return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i=0; i<matrix.GetLength(0);i++)
+{
+    for (int j=0;j<matrix.GetLength(1);j++)
+    {
+        System.Console.Write(matrix[i,j]+"\t");
+    }
+    System.Console.WriteLine();
+}
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = MatrixSumIndex(m,n);
+System.Console.WriteLine();
+
+PrintMatrix(matrix);
