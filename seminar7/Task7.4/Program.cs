fcb18967ca89490be846f824436ab9287e6e738d
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-02-09 21:27:23 +0500

    Added Task7.4

---
+++ seminar7/Task7.4/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали
+// (с индексами (0,0); (1,1) ...)
+
+Console.Clear();
+
+int[,] MatrixSumIndex(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumDiagonalMatrix(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = MatrixSumIndex(m, n);
+System.Console.WriteLine();
+
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+System.Console.WriteLine(SumDiagonalMatrix(matrix));
+
