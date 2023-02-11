d6145ba41c71ea1516cbdb3c00dbe9a863cedb24
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-02-11 18:12:37 +0500

    Added Task8.1

---
+++ seminar8/Task8.1/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива
+
+
+Console.Clear();
+
+uint EnterNumberNatur(string messeng)
+
+{
+    uint number;
+    Console.Write(messeng);
+    while (true)
+    {
+        if (!(uint.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+            Thread.Sleep(1000);
+            Console.CursorTop--;
+            System.Console.WriteLine("                                               ");
+            Console.CursorTop--;
+            Console.CursorTop--;
+            System.Console.WriteLine("                                               ");
+            Console.CursorTop--;
+            Console.Write(messeng);
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+
+}
+
+int[,] FillMatrix(uint rows, uint cols)
+{
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return matrix;
+}
+
+void MatrixChange(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+uint rows = EnterNumberNatur("Введите количество строк матрицы: ");
+uint cols = EnterNumberNatur("Введите количество столбцов матрицы: ");
+int[,] matrix = FillMatrix(rows, cols);
+System.Console.WriteLine();
+
+System.Console.WriteLine("Задан массив:");
+System.Console.WriteLine();
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+MatrixChange(matrix);
+PrintMatrix(matrix);
