426236750edfc9e8438563ee83536afa60099e07
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-03-10 23:33:22 +0500

    Added Task9.3

---
+++ seminar9/Task9.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать
+//  на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+
+int Sum(int numb)
+{
+
+    if (numb == 0)
+    {
+        return 0;
+    }
+    return numb % 10 + Sum(numb / 10);
+}
+
+System.Console.WriteLine($"{Sum(453)}");
