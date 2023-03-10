dc9f752a14e1bcfb8eefd0ce53ddb9d5495e65b7
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-03-10 23:33:31 +0500

    Added Task9.4

---
+++ seminar9/Task9.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход 
+// принимает два числа A и B, и возводит число А в
+// целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+
+int NumberInPower(int numb1, int numb2)
+{
+
+    if (numb2 > 1)
+        return numb1 * NumberInPower(numb1, numb2 - 1);
+    return numb1;
+}
+
+System.Console.WriteLine($"{NumberInPower(5, 3)}");
