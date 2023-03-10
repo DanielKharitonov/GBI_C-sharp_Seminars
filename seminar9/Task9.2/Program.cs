427c1d3c316cb371f2889db8dc67aaec647a4adf
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-03-10 23:33:14 +0500

    Added Task9.2

---
+++ seminar9/Task9.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке
+// от M до N
+
+Console.Clear();
+
+void PrintFromNumberToOne(int numb1, int numb2)
+{
+    if (numb2 > numb1)
+    {
+        PrintFromNumberToOne(numb1, numb2 - 1);
+    }
+    Console.WriteLine(numb2);
+}
+
+PrintFromNumberToOne(4,8);
