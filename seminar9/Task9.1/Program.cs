47c2cc20b125347e4d89abedc330e6d5c8a56e7e
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2023-03-10 23:33:03 +0500

    Added Task9.1

---
+++ seminar9/Task9.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке
+// от 1 до N
+
+Console.Clear();
+
+void PrintFromNumberToOne(int numb)
+{
+    if (numb > 1)
+    {
+        PrintFromNumberToOne(numb - 1);
+    }
+    Console.WriteLine(numb);
+}
+
+PrintFromNumberToOne(5);
